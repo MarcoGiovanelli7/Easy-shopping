0a7b139b430137aaec9eb791cb339eb3238dffb8
MarcoGiovanelli7 <marckmirr@gmail.com>
2025-07-14 10:52:23 -0300

    ediçao de const

--- Javascript/2.tiposdedadosprt2.js
+++ Javascript/2.tiposdedadosprt2.js
@@ -1,7 +1,8 @@
 const myAge = 55
 const myCar = 'fordKa'
+const mywife = 'Roberta'
 
 const myString = `minha idade é ${myAge},
-eu tenho um carro ${myCar}`
+eu tenho um carro ${myCar},eu tenho uma linda esposa que se chama ${mywife}  `
 
 console.log(myString)
