410b581b37664a657b234888d1eabfcbcd9456a1
MarcoGiovanelli7 <marckmirr@gmail.com>
2025-07-21 12:28:12 -0300

    function

---
+++ Javascript/functions.js
@@ -0,0 +1,12 @@
+/*Functions / funcoes
+     Trecho de coddigo que é executado
+     Somente quando solicitado(parente gente boa)
+*/
+   const variavel= "abacatinho"
+    
+   console.log(variavel)
+   function nomenatela() {
+
+    console.log("Giovanelli")
+   }
+    nomenatela() //chamando a função
