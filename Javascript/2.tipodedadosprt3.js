043f28f1b05cd9f3c23cee522860958b01dc53cb
MarcoGiovanelli7 <marckmirr@gmail.com>
2025-07-21 09:24:00 -0300

    correçao

--- Javascript/2.tipodedadosprt3.js
+++ Javascript/2.tipodedadosprt3.js
@@ -1,3 +1,10 @@
-const name = "tMarco Aurelio"
+const name = "Marco Aurelio"
 const age = 55  
-const address = "minha,numero 7"
+const address = "minha rua,numero 7"
+
+const Marco = {
+    name: "Marco",
+    age: 55,
+    address: "minha rua,numero 7"   
+}
+console.log(Marco)
