73fa4ca2551e010b4bb2f2f89c789ba871747adf
MarcoGiovanelli7 <marckmirr@gmail.com>
2025-07-25 11:11:54 -0300

    crr

--- Javascript/script.js
+++ Javascript/script.js
@@ -3,7 +3,9 @@ const input = document.querySelector('#main-input')
 const select = document.querySelector('select')
 const button = document.querySelector('.main-button')
 
-select.addEventListener("change", function(){   
-console.log("troquei de valor")
-})
+function troqueiValor(abacate) {
+    console.log(abacate)
+}
+
+button.addEventListener("click", troqueiValor)
 
