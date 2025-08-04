f3a61c810782ebab9b102e28e39d9a9bfb297004
MarcoGiovanelli7 <marckmirr@gmail.com>
2025-08-04 11:35:31 -0300

    up no conversor

--- Devclub-coversor-de-moedas/assets/script.js
+++ Devclub-coversor-de-moedas/assets/script.js
@@ -1,3 +1,28 @@
-const convertButton = document.querySelector(".convert-button")
+const converterButton = document.querySelector(".convert-button")
 
-console.log(convertButton)
+function convertValues() {
+    const inputCurrencyValue = document.querySelector(".input-currency").value
+    const currencyValueToConvert = document.querySelector(".currency-value-to-convert")//valor em real
+    const currencyValueConverted = document.querySelector(".currency-value")//valor em dolar
+
+    const dolarToday = 5.2
+
+    const convertedValue = inputCurrencyValue / dolarToday
+
+    currencyValueToConvert.innerHTML = new Intl.NumberFormat('pt-BR', {
+        style: 'currency',
+        currency: 'BRL' 
+    }).format(inputCurrencyValue)
+
+    currencyValueConverted.innerHTML = new Intl.NumberFormat('en-US', {
+        style: 'currency',
+        currency: 'USD'
+    }).format(convertedValue)   
+
+    console.log(convertedValue)
+
+    
+}
+
+converterButton.addEventListener("click",convertValues)
+
