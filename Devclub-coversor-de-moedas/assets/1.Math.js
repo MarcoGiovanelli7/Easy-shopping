db4f3151975f26d7a99df4489ce147d9f7c7bc29
MarcoGiovanelli7 <marckmirr@gmail.com>
2025-09-09 11:23:01 -0300

    Projeto Random

---
+++ Devclub-coversor-de-moedas/assets/1.Math.js
@@ -0,0 +1,15 @@
+/*
+
+
+Math ->Matemática 
+-pow(2,2) / potência / 2² = 4
+-sqrt(25,2) / raiz / √25
+-PI
+-ceil / teto -> arredonda para cima
+-floor / chão -> arredonda para baixo
+-random -> número aleatório entre 0 e 1
+
+*/
+
+const result = Math.random()
+console.log(result)
