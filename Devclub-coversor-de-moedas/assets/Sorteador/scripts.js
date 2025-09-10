21726797275de90376c1d4775f5aa6cc11a507d7
MarcoGiovanelli7 <marckmirr@gmail.com>
2025-09-10 10:41:35 -0300

    acerto

--- Devclub-coversor-de-moedas/assets/Sorteador/scripts.js
+++ Devclub-coversor-de-moedas/assets/Sorteador/scripts.js
@@ -1,3 +1,13 @@
 function generateNumber() {
-    console.log("Botão clicado");
+  
+    const min = Math.ceil(document.querySelector(".input-min").value)
+    const max = Math.floor(document.querySelector(".input-max").value)
+
+    const result = Math.floor(Math.random() * (max - min + 1)) + min;
+
+    alert(result)
+
+   
+
+
 }
