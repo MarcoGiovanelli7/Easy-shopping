4cd57fa53ac2784dcc6b281fb4b02bbfa0ebbf35
MarcoGiovanelli7 <marckmirr@gmail.com>
2025-09-15 11:59:49 -0300

    novo arquivo

---
+++ Devclub-coversor-de-moedas/assets/Sorteador/2.Operadores-aritimeticos.js
@@ -0,0 +1,21 @@
+/*
+
+
+->operadores aritméticos
+
+->+ adição
+->- subtração
+->/ divisão
+->* multiplicação 
+->%resto
+->++ incremento
+->-- decremento
+->** exponecial
+
+*/ 
+
+let result = 5 ** 10
+
+console.log(result)
+
+
