01b2f8b37c9b05acedb4817f34c0cc0d30e2c16a
MarcoGiovanelli7 <marckmirr@gmail.com>
2025-09-22 10:03:23 -0300

    correcoes e nova aula

---
+++ Devclub-coversor-de-moedas/assets/Sorteador/3Operadores-aritimeticos.js
@@ -0,0 +1,18 @@
+/*
+
+
+Operadores de Atribuição
+
+= -> Atribuição
++= -> Adição
+-=-> Subtração
+*= -> Multiplicação
+/= -> Divisão
+%= -> Resto 
+
+*/
+
+let result = 20 
+result %= 3 // result = result + 10
+
+console.log(result)
---
+++ Devclub-coversor-de-moedas/assets/Sorteador/OperadoresdeComparçao.js
@@ -0,0 +1,29 @@
+/*
+
+
+Operadores de Comparação
+== -> Igual // Compara o valor,mas não o tipo
+=== -> Idêntico // Compara o valor e o tipo
+
+!= -> Diferente // Compara o valor,mas não o tipo
+!== -> Não idêntico // Compara o valor e o tipo
+
+
+*/
+
+const firstNumber = 30
+const secondNumber = "30"
+
+console.log(firstNumber != secondNumber) // true
+
+
+
+
+
+
+
+if(firstNumber != secondNumber){
+    console.log("Os valores são diferentes")
+}else {
+    console.log("Não, Os valores não são diferentes")
+}
