be5a2ab738802616273989c18c5e22f141374301
MarcoGiovanelli7 <marckmirr@gmail.com>
2025-09-29 11:28:38 -0300

    operador ternario

---
+++ Devclub-coversor-de-moedas/assets/Sorteador/Operador-ternário.js
@@ -0,0 +1,18 @@
+/*
+Operador Ternário
+
+? if (se)
+: else (se não)
+&& (if sem else)
+
+*/
+const salary = 5000
+
+  //  if (salary < 3000) {
+  //     console.log('Você é um iniciante')
+  // }else {
+  //    console.log('Você é um senior') }
+
+   
+  
+  salary < 3000 ? console.log('Você é um iniciante') : console.log('Você é um senior')
