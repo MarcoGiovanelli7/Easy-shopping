6ed0fe2f54a79aa91e787fe8b1d3c1f20510c51b
MarcoGiovanelli7 <marckmirr@gmail.com>
2025-09-29 09:52:56 -0300

    operador typeof

---
+++ Devclub-coversor-de-moedas/assets/Sorteador/Operador-type-of-delete.js
@@ -0,0 +1,18 @@
+/*
+
+
+ Operadores Type of e Delete
+
+ typeof -> tipo do dado
+ delete ->
+
+*/
+
+const myObject = {
+    name: "Marco",
+    age: 55,
+    address:"Rua ABC, 123"
+}
+delete myObject.address
+
+console.log( myObject)
