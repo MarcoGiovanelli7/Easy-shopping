ec1245f2679085304e1c29beeff35b9ce9606ac4
MarcoGiovanelli7 <marckmirr@gmail.com>
2025-10-06 10:37:25 -0300

    Switch Case

---
+++ Devclub-coversor-de-moedas/assets/Sorteador/Switch Case.js
@@ -0,0 +1,23 @@
+/*
+ Switch Case -Controlor de Fluxo
+*/
+const user = 'Giovanna'
+
+switch (user) {
+    case "Marco":
+        console.log('eu')
+        break
+
+    case "Roberta":
+        console.log('minha esposa')
+        break
+
+    case "Giovanna":
+        console.log('tá quente minha filha')
+        break
+
+    
+    default:
+        console.log('Não sei te dizer')
+
+}
