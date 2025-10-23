2227c28e4961425d881b42de9effa89fd4327133
MarcoGiovanelli7 <marckmirr@gmail.com>
2025-10-23 10:02:39 -0300

    Do while

---
+++ CSS/Aulas de javascript/4.Do While.js
@@ -0,0 +1,15 @@
+/*
+   while & do while
+
+    while -> verifique se é TRUE e Faça 
+    do while -> Faça e verifique se é TRUE
+
+*/
+let i = 0
+
+ do  {
+    i++
+    console.log(i)  
+
+
+ } while  (i < 10);
