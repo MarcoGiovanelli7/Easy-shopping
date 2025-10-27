53fae31e36f19ce56d59cee6ff02684b0465a6f0
MarcoGiovanelli7 <marckmirr@gmail.com>
2025-10-27 11:09:17 -0300

    functions.js

---
+++ CSS/Aulas de javascript/Functions.js
@@ -0,0 +1,40 @@
+/*  Functions/funções
+        Trecho de código que é executado
+        Somente quando solicitado (parente gente boa)
+*/
+
+/*const variavel ="Formação"
+
+console.log(variavel)
+
+function nomeNaTela(){
+    console.log("Marco")
+}
+nomeNaTela()
+*/
+
+/*function saymyName(name){
+    console.log(`o nome é: ${name}`)
+}
+saymyName("Marco")
+saymyName("João")
+saymyName("Maria")*/
+
+/*function sum(valor, valor2){
+    console.log(valor + valor2)
+}
+sum(20,30)
+sum(5,15)
+sum(100,200)*/
+
+/*function saymyName(name = "Marco"){
+    console.log(name)
+}
+saymyName()
+*/
+
+function sum(valor = 7, valor2 = 1){
+    console.log(valor + valor2)
+
+}
+sum(23,27)
