065f52b425bfbac3dc5a8533f2edd399875e64f3
MarcoGiovanelli7 <marckmirr@gmail.com>
2025-11-12 14:03:30 -0300

    Map 2

--- CSS/Aulas de javascript/Projeto js jokenpow/6-Map.js
+++ CSS/Aulas de javascript/Projeto js jokenpow/6-Map.js
@@ -11,6 +11,38 @@ const number = [1, 2, 3, 4, 5,6,7]
 
 const double = number.map((item) =>  item * 2)    
    
-   
+   const list = [
+    {name: 'Marco', vip: true},
+    {name: 'José', vip: false},
+    {name: 'Maria', vip: true},
+    {name: 'João', vip: true},
+    {name: 'Ana', vip: false},
+    {name: 'Pedro', vip: true},
+    {name: 'Carla', vip: false}
+   ]
+  const newlist = list.map(user => {
+    const newUser = {
+        name: user.name,
+        braceletColor: user.vip ? 'black' : 'green'
+}
+    return newUser
+})
+ const students = [
+    {name: 'Marco', testGrade: 7},
+    {name: 'José', testGrade: 4},
+    {name: 'Maria', testGrade: 8},
+    {name: 'João', testGrade: 6},
+    {name: 'Ana', testGrade: 3},
+    {name: 'Pedro', testGrade: 9},
+    {name: 'Carla', testGrade: 5}
+   ]
+  const newstudent = students.map(student => {
+    const newStudent = {
+        name: student.name,
+        finalResult: student.testGrade >= 5.5 ? 'Aprovado' : 'Reprovado'
+        
+}   
+    return newStudent
 
-console.log(double)
+  })
+  console.log(newstudent)
