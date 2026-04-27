3a6541f40a00533198c6544656f0cff16f8f7717
Marco Giovanelli <marckmirr@gmail.com>
2026-04-27 11:44:18 -0300

    limpeza

--- Devclub-coversor-de-moedas/scripts.js
+++ Devclub-coversor-de-moedas/scripts.js
@@ -1,3 +1,65 @@
 function generateNumber() {
     console.log("Botão clicado");
 }
+const convertButton = document.querySelector(".convert-button")
+const currencySelect = document.querySelector(".currency-select")
+
+const convertValues = async () => {
+    const inputCurrencyValue = parseFloat(document.querySelector(".input-currency").value.replace(",", "."))
+    const currencyValueToConvert = document.querySelector(".currency-value-to-convert")
+    const currencyValueConverted = document.querySelector(".currency-value")
+
+    const data = await fetch("https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,GBP-BRL")
+        .then(Response => Response.json())
+
+    const dolarToday = data.USDBRL.high
+    const euroToday = data.EURBRL.high 
+    const libraToday = data.GBPBRL.high 
+    console.log(data)
+
+
+
+    if (currencySelect.value === 'dolar') {
+        currencyValueConverted.innerHTML = new Intl.NumberFormat('en-US', {
+            style: "currency",
+            currency: "USD"
+        }).format(inputCurrencyValue / dolarToday)
+    }
+
+    if (currencySelect.value === "euro") {
+        currencyValueConverted.innerHTML = new Intl.NumberFormat("de-DE", {
+            style: "currency",
+            currency: "EUR"
+        }).format(inputCurrencyValue / euroToday)
+    }
+
+
+
+
+    currencyValueToConvert.innerHTML = new Intl.NumberFormat('pt-BR', {
+        style: "currency",
+        currency: "BRL"
+    }).format(inputCurrencyValue)
+}
+
+function changeCurrency() {
+    const currencyName = document.querySelector("#currency-name")
+    const currencyImg = document.querySelector(".currency-img")
+
+    if (currencySelect.value === "dolar") {
+        currencyName.innerHTML = "Dólar americano"
+        currencyImg.src = "./assets/Dolar.png"
+    }
+
+    if (currencySelect.value === "euro") {
+        currencyName.innerHTML = "Euro"
+        currencyImg.src = "./assets/Design sem nome 3.png"
+    }
+    if (currencySelect.value === "libra") {
+        currencyName.innerHTML = "libra esterlina"
+        currencyImg.src = "./assets/libra 1.png"
+    }
+}
+
+currencySelect.addEventListener("change", changeCurrency)
+convertButton.addEventListener("click", convertValues)
